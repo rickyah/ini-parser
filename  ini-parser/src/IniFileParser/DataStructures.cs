0e1981bb1931cf72990e6735b1c162b8674d6b59
rickyah <rickyah@13cfbc6d-2254-0410-8834-c1f1d55d7b3c>
2008-10-05 18:37:00 +0000

    git-svn-id: https://ini-parser.googlecode.com/svn/trunk@20 13cfbc6d-2254-0410-8834-c1f1d55d7b3c

---  ini-parser/src/IniFileParser/DataStructures.cs
+++  ini-parser/src/IniFileParser/DataStructures.cs
@@ -764,6 +764,12 @@ namespace IniParser
 
         #region ICloneable Members
 
+        /// <summary>
+        /// Creates a new object that is a copy of the current instance.
+        /// </summary>
+        /// <returns>
+        /// A new object that is a copy of this instance.
+        /// </returns>
         public object Clone()
         {
             return new IniData(this.Sections);
