d49baa7362a4c8e8a6e53c39fa59fb284fb45cb5
rickyah <rickyah@13cfbc6d-2254-0410-8834-c1f1d55d7b3c>
2008-10-31 20:30:56 +0000

    Created new file for holding Issues tests
    git-svn-id: https://ini-parser.googlecode.com/svn/trunk@31 13cfbc6d-2254-0410-8834-c1f1d55d7b3c

---
+++  ini-parser/src/IniFileParserTests/Issues_Tests.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using IniParser;
+
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+
+namespace IniParserTestNamespace
+{
+    [TestFixture]
+    public class Issues_Test
+    {
+        #region Test Members
+
+        //TODO: Add fields used in the tests
+
+        #endregion
+
+        #region TestFixture SetUp/TearDown
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            //TODO: Add test fixture set up code
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            //TODO: Add test fixture tear down code
+        }
+
+        #endregion
+
+        #region  Test SetUp/TearDown
+
+        [SetUp]
+        public void SetUp()
+        {
+            //TODO: Add test set up code
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            //TODO: Add test tear down code
+        }
+
+        #endregion
+
+        #region Tests
+
+        [Test, Description("Test for Issue 2: http://code.google.com/p/ini-parser/issues/detail?id=2")]
+        public void Issue2Test()
+        {
+            string test = "[ExampleSection]\nkey = value;value\n";
+
+            StringIniParser strParser = new StringIniParser();
+
+            IniData data = strParser.ParseString(test);
+
+            Assert.That(data.Sections.Count, Is.EqualTo(1));
+            Assert.That(data.Sections["ExampleSection"], Is.Not.Null);
+            Assert.That(data.Sections["ExampleSection"].Count, Is.EqualTo(1));
+            Assert.That(data.Sections["ExampleSection"]["key"], Is.EqualTo("value"));
+
+
+            FileIniDataParser fParser = new FileIniDataParser();
+            data = fParser.LoadFile(@"../../INIFileBAD_Issue2.ini");
+
+            Assert.That(data.Sections.Count, Is.EqualTo(1));
+            Assert.That(data.Sections["ExampleSection"], Is.Not.Null);
+            Assert.That(data.Sections["ExampleSection"].Count, Is.EqualTo(1));
+            Assert.That(data.Sections["ExampleSection"]["key"], Is.EqualTo("value"));
+        }
+
+        [Test, Description("Test for Issue 3: http://code.google.com/p/ini-parser/issues/detail?id=3")]
+        public void Issue3_Tests()
+        {
+            string strTest = "[section.issue3]\nkey.with.dots = value\n";
+
+            IniData
+        }
+
+
+
+        #endregion
+    }
+}
