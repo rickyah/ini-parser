1851e8cfa282f00b17ab72b94ced6a20d762fbed
Ricardo Amores <rickyah@gmail.com>
2012-04-27 00:43:37 +0200

    This test was actually intended for issue 26
    http://code.google.com/p/ini-parser/issues/detail?id=26

--- src/IniFileParser.Tests/issues/Issue26Tests.cs
+++ src/IniFileParser.Tests/issues/Issue26Tests.cs
@@ -6,9 +6,9 @@ using NUnit.Framework.SyntaxHelpers;
 namespace IniFileParser.Tests.issues
 {
     [TestFixture]
-    public class Issue11Tests
+    public class Issue26Tests
     {
-        [Test, Description("Test for Issue 11: http://code.google.com/p/ini-parser/issues/detail?id=11")]
+        [Test, Description("Test for Issue 26: http://code.google.com/p/ini-parser/issues/detail?id=26")]
         public void allow_duplicated_sections()
         {
             FileIniDataParser parser = new FileIniDataParser();
