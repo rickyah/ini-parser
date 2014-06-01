32f34182a763a12234e1fa06b58239e07524f122
Ricardo Amores <rickyah@gmail.com>
2014-06-01 12:18:57 +0200

    Add documentation

--- src/IniFileParser/Model/Formatting/IIniDataFormatter.cs
+++ src/IniFileParser/Model/Formatting/IIniDataFormatter.cs
@@ -13,6 +13,11 @@ namespace IniParser.Model.Formatting
         /// <returns>The data to string.</returns>
         /// <param name="iniData">Ini data.</param>
         string IniDataToString(IniData iniData);
+
+        /// <summary>
+        ///     Configuration used by this formatter when converting IniData
+        ///     to an string
+        /// </summary>
         IIniParserConfiguration Configuration {get;set;}
     }
     
