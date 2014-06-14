805d5b4c1fd52baa19b2d36d2268e2af58420b9b
Ricardo Amores <rickyah@gmail.com>
2014-06-14 10:42:22 +0200

    Remove ignored test now obsolete

--- src/IniFileParser.Tests/issues/Issue6Tests.cs
+++ src/IniFileParser.Tests/issues/Issue6Tests.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using IniParser;
 using IniParser.Model;
 using NUnit.Framework;
@@ -13,18 +11,6 @@ namespace IniFileParser.Tests.issues
         /// <summary>
         ///     Thanks to h.eriksson@artamir.org for the issue.
         /// </summary>
-        [Test, Description("Test for Issue 6: http://code.google.com/p/ini-parser/issues/detail?id=6")]
-        [Ignore("Deprecated: parsing comment characters behaviour changed")]
-        public void check_that_comment_char_is_not_stored_as_the_key_old()
-        {
-            string data = "[data]" + Environment.NewLine + "key = value;";
-
-            IniData inidata = new StringIniParser().ParseString(data);
-
-            Assert.That(inidata["data"]["key"], Is.EqualTo("value"));
-        }
-
-
         [Test, Description("Test for Issue 6: http://code.google.com/p/ini-parser/issues/detail?id=6")]
         public void check_that_comment_char_is_not_stored_as_the_key()
         {
