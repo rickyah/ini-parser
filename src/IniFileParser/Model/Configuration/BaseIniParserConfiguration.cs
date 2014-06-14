e52bfeecd575988173cd86f91b1eb063919ceda7
Ricardo Amores <rickyah@gmail.com>
2014-06-14 10:36:10 +0200

    Now sections can have asterisks in its name

--- src/IniFileParser/Model/Configuration/BaseIniParserConfiguration.cs
+++ src/IniFileParser/Model/Configuration/BaseIniParserConfiguration.cs
@@ -203,7 +203,7 @@ namespace IniParser.Model.Configuration
         #region Constants
         protected const string _strCommentRegex = @"^{0}(.*)";
         protected const string _strSectionRegexStart = @"^(\s*?)";
-        protected const string _strSectionRegexMiddle = @"{1}\s*[_\{\}\#\+\;\%\(\)\=\?\&\$\,\:\/\.\-\w\d\s\\\~]+\s*";
+        protected const string _strSectionRegexMiddle = @"{1}\s*[_\{\}\#\+\;\*\%\(\)\=\?\&\$\,\:\/\.\-\w\d\s\\\~]+\s*";
         protected const string _strSectionRegexEnd = @"(\s*?)$";
         protected const string _strKeyRegex = @"^(\s*[_\.\d\w]*\s*)";
         protected const string _strValueRegex = @"([\s\d\w\W\.]*)$";
