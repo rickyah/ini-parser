13b5880e3378dea7a58e5f436908b186606c6ddc
Ricardo Amores <rickyah@gmail.com>
2015-06-06 21:40:39 +0200

    Add a link to the AssemblyInfo file in IniParser.Tests project

--- src/IniFileParser/Properties/AssemblyInfo.cs
+++ src/IniFileParser/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.0.1")]
-[assembly: AssemblyVersion("2.2.0.1")]
+[assembly: AssemblyVersion("2.2.1")]
+[assembly: AssemblyFileVersion("2.2.1")]
