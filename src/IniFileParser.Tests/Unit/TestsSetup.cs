b080607a53742b0520e7b112ed417567a5d71881
Richard Schwab <mail@w.tf-w.tf>
2017-04-16 02:05:38 +0200

    Update to NUnit 3 (#142)
    * Update to NUnit 3
    
    * Change obsolete APIs
    * Fix test running both locally and on travis

---
+++ src/IniFileParser.Tests/Unit/TestsSetup.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using NUnit.Framework;
+
+namespace IniFileParser.Tests
+{
+    [SetUpFixture]
+    public class TestsSetup
+    {
+        [OneTimeSetUp]
+        public void SetWorkingDirForTest()
+        {
+            Directory.SetCurrentDirectory(TestContext.CurrentContext.TestDirectory);
+        }
+    }
+}
