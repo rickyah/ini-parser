23f9f76a7eee6adf04109a0f18ec6bef7ed58744
Ricardo Amores <rickyah@gmail.com>
2019-05-21 22:23:27 +0200

    Build and test with mono and dotnetcore

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Running install script for BUILD_TYPE: $BUILD_TYPE"
+echo
+
+if [ "$BUILD_TYPE" = "mono" ]; then
+    set -x
+    nuget restore src/IniFileParser.sln
+    echo
+    nuget install NUnit.Console -Version 3.10.0 -OutputDirectory testrunner
+    echo
+elif [ "$BUILD_TYPE" = "dotnetcore" ]; then
+    set -x
+    dotnet restore src/IniFileParser.sln
+    echo
+else
+    echo "ERROR: Unknown build type value: $BUILD_TYPE"
+    exit -1
+fi
